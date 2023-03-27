d34a76e7101a2230fc60bbd33e7468b06cf56fe2
Diogenes Monegro <diogenes_mj@hotmail.com>
2023-03-27 13:16:13 -0400

    feat: new custom group for camara

--- src/sprites/block.py
+++ src/sprites/block.py
@@ -2,9 +2,9 @@ import pygame
 from settings import *
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y) -> None:
-        super().__init__()
+    def __init__(self, pos:set(), groups) -> None:
+        super().__init__(groups)
         self.image = pygame.surface.Surface((TITLESIZE, TITLESIZE))
         self.image.fill((0,0,0))
         self.rect = self.image.get_rect()
-        self.rect.topleft = (pos_x, pos_y)
+        self.rect.topleft = pos
