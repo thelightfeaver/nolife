79a9940e315f7ddcb52b27735c341a5011d92f75
diogenes69 <diogenes_monegro@claro.com.do>
2023-04-03 18:57:44 -0400

    chore: change color background

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,9 +1,9 @@
 import pygame 
 from settings import FONT_NAME, WHITE
 
-def draw_text(surface, text, size, x, y):
+def draw_text(surface, text, size, pos):
     font = pygame.font.Font(FONT_NAME, size)
-    text_surface = font.render(text, True, WHITE)
+    text_surface = font.render(text, True, "green", "black" )
     text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
+    text_rect.midtop = pos
     surface.blit(text_surface, text_rect)
