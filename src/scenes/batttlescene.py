0be3de9cd6e2797697e9453b5cf42d3324346fec
diogenes69 <diogenes_monegro@claro.com.do>
2023-04-03 20:20:28 -0400

    feat: add collision between player and enemy and implement recharge of shoot

--- src/scenes/batttlescene.py
+++ src/scenes/batttlescene.py
@@ -1,5 +1,4 @@
 import random
-import time
 
 import pygame
 
@@ -10,6 +9,7 @@ from sprites.enemy import Enemy
 from sprites.player import Player
 from sprites.block import Block
 from settings import *
+from utils.util import draw_text
 
 
 class BattleScene:
@@ -17,7 +17,7 @@ class BattleScene:
         # Get surface
         self.screen = pygame.display.get_surface()
 
-        # groups sprite
+        # Groups sprite
         self.bullets_sprite = pygame.sprite.Group()
         self.enemies_sprite = pygame.sprite.Group()
         self.obtacles_sprite = pygame.sprite.Group()
@@ -44,31 +44,45 @@ class BattleScene:
                     Block((x, y), [self.visible_sprite, self.obtacles_sprite])
                 
     def spawn_enemy(self, count):
+        # Spawn enemy x times
         for _ in range(count):   
-            Enemy((random.randint(100, WIDTH - 100 ) , 
-                   random.randint(100, HEIGHT - 100 )), 
+            Enemy((random.randint(100, WIDTH - 100), 
+                   random.randint(100, HEIGHT - 100)), 
                    [self.visible_sprite, self.enemies_sprite])
 
     def run(self):
         self._draw()
         self._input()
         self._colission()
+        draw_text(self.screen, f"HP: {self.player.get_hp()}", 20, (WIDTH - 100, 10))
        
     def _draw(self):
+        # Draw all sprites
         self.visible_sprite.custom_draw(self.player)
         self.visible_sprite.update(player = self.player)
 
     def _input(self):
-        if pygame.mouse.get_pressed()[0]:
-            mouse_pos = pygame.mouse.get_pos()
-            Bullet(self.player.rect.center, mouse_pos + self.visible_sprite.offset, [self.visible_sprite, self.bullets_sprite])
-   
+        # Validate if player is ready to recharge
+        if self.player.ready_recharge:
+            # Reset recharge
+            self.player.reset_recharge()
+            # Validate click left
+            if pygame.mouse.get_pressed()[0]:
+                # Get mouse position
+                mouse_pos = pygame.mouse.get_pos()
+                # Create bullet
+                Bullet(self.player.rect.center, mouse_pos + self.visible_sprite.offset, [self.visible_sprite, self.bullets_sprite])
+
     def _colission(self):
         # Collision between bullets and enemies
-        hits = pygame.sprite.groupcollide(self.enemies_sprite, self.bullets_sprite, False, True)
+        enemies = pygame.sprite.groupcollide(self.enemies_sprite, self.bullets_sprite, False, True)
         
-        for hit in hits:
-            hit.get_damage(5)
+        for enemy in enemies:
+            enemy.get_damage(5)
 
         # Collision between bullets and obtacles
         pygame.sprite.groupcollide(self.bullets_sprite, self.obtacles_sprite, True, False)
+
+        # Collision between player and enemies
+        if pygame.sprite.spritecollide(self.player, self.enemies_sprite, False):
+            self.player.get_damage(5)
